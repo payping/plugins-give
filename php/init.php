e1d4ae7f6f11e9d92da155afcf8f706126f7aea4
Erfan Ebrahimi <persionhost@gmail.com>
2018-12-17 12:02:58 +0330

    Create init.php

---
+++ php/init.php
@@ -0,0 +1,9 @@
+<?php
+    
+$wordpressDir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
+
+require_once($wordpressDir . '/wp-load.php');
+require_once($wordpressDir . '/wp-settings.php');
+require_once($wordpressDir . '/wp-includes/ms-functions.php');
+require_once($wordpressDir . '/wp-includes/class-phpass.php');
+
